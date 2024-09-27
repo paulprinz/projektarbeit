249b477037dc364e3716b6054c6408b0f5b5c47c
JanosM99 <janos.molnar18@gmail.com>
2024-09-27 10:33:57 +0200

    Fixes

--- test/populate_database.sql
+++ test/populate_database.sql
@@ -1,10 +1,10 @@
 INSERT INTO user (user_name, active, birth_date, follower_count, id, country, email, password, role)
 VALUES
-    ('a',true,'2024-09-22',0,1,'Great Britain','a@email.com','$2a$10$8Vhl6Ac785V01wyhmeJj/eUu6.ykGOXpeE1O7ZgpmoeERrNdvnoB2','ROLE_ADMIN'),
-    ('b',true,'2024-09-22',1,2,'Austria','b@email.com','$2a$10$8TXEKxaEi5BKtuFhUDH8UOpaa96xHNUjxzUHaMEpysjw5O6DAIvxi','ROLE_ADMIN'),
-    ('c',true,'2024-09-22',2,3,'Hungary','c@email.com','$2a$10$XZPjO/1pJL83NZ08XuQvkOFFGoIZ0ANCBllTN263qO6BQPEGaZKiC','ROLE_ADMIN'),
-    ('d',false,'1945-10-09',4,4,'Germany','d@supermail.it','$2a$10$qaYcNzTBwHWgzT7ayZ/t/.y9Du.wbO2/iqQkWVp4WGGp8XQnD221.','ROLE_USER'),
-    ('e',false,'2021-06-07',0,5,'Luxembourg','e@gmail.com','$2a$10$4AJE5surM8XupFuFRZ/y0eAT55bS48hCVr/oDsUVhlg1Lzox97hF.','ROLE_USER'),
-    ('f',true,'2024-09-22',100,6,'Austria','f@email.at','$2a$10$CML7YUXO.xPyBJud8Y/V9OTMN5dV8j8cjfyPmVKL.7vtMwD.i98ES','ROLE_USER'),
-    ('g',true,'2024-09-08',0,7,'USA','g@gmail.com','$2a$10$wifRwwZDVloYQrgjG9kc1udppzhoP952Yo1aIFBeFP6TeClEvsZdC','ROLE_USER'),
-    ('h',false,'2024-09-22',0,11,'Austria','h@email.com','$2a$10$tayJIU9A.OZIuvzcKKSTm.VEG3fDtuIfRbJTou7UrpLC8bIcX35JG','ROLE_USER');
+    ('dev1',true,'2024-09-22',0,1,'Great Britain','dev1@email.com','$2a$10$EAwsItTxg6qPyErNEk.0mu6AQKLp4zh3XRuOghbhERAJ7akFVG4ta','ROLE_ADMIN'),
+    ('dev2',true,'2024-09-22',1,2,'Austria','dev2@email.com','$2a$10$Ev/uL.AOuOiVSPCQCdfQkOMRwHZbuyTFvJ2TnZTSClt.KpBKyybdW','ROLE_ADMIN'),
+    ('dev3',false,'2024-09-22',2,3,'Hungary','dev3@email.com','$2a$10$Xt96FEagIGGUX3lW5u3V7uNp49Gmw6F7HBaMUggyOnLqnfGGv38sm','ROLE_ADMIN'),
+    ('user1',true,'1945-10-09',4,4,'Germany','user1@supermail.it','$2a$10$CLG5dj79iXvv35GjHBQ/3eY0MvqPr4jq5dLgBlqKyXIt3xKERbRCu','ROLE_USER'),
+    ('user2',true,'2021-06-07',0,5,'Luxembourg','user2@gmail.com','$2a$10$.ILU.qq32dUtcme6vd.atOKmN0zdkC8bmetp8Nx4CvEWLKTbel3bW','ROLE_USER'),
+    ('user3',true,'2024-09-22',100,6,'Austria','user3@email.at','$2a$10$3./j9mZl60QesCnKKzjsXOBSjuugG1SngDK3gDH/qQ35hoD9wUQA6','ROLE_USER'),
+    ('user4',false,'2024-09-08',0,7,'USA','user4@gmail.com','$2a$10$M56x6pvFvUY0SJQ9OoV9qOH2XA4DnUNmReKBUErJSFb6lfGaVl3r2','ROLE_USER'),
+    ('user5',false,'2024-09-22',0,8,'Austria','user5@email.com','$2a$10$6aXHSlEd1AhgDxVfBPTLIeG2gZ8oJ6xh8SIet/tLq1PzRZBc/iRE2','ROLE_USER');
